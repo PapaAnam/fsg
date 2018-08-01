8b8ef646ebad2d53448d500cf55e64f2d814f896
PapaAnam <hairulanam21@outlook.com>
2018-08-01 19:06:06 +0700

    add email in leftbar

--- resources/views/leftbar.blade.php
+++ resources/views/leftbar.blade.php
@@ -8,7 +8,7 @@
         <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
       </div>
       <div class="pull-left info">
-        <p class="text-capitalize">{{ Auth::user()->nama_lengkap }}</p>
+        <p>{{ substr(Auth::user()->email, 0, 16) }}</p>
         <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
       </div>
     </div>
